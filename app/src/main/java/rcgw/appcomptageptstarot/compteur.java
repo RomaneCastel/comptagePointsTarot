16ec41f07e979f988180d40f3aef9ed7eba15a4a
Romydiboubou <romane.castel@outlook.fr>
2018-03-15 21:53:54 +0100

    replace buttons by toggle buttons

--- app/src/main/java/rcgw/appcomptageptstarot/compteur.java
+++ app/src/main/java/rcgw/appcomptageptstarot/compteur.java
@@ -6,26 +6,52 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 
 public class compteur extends AppCompatActivity {
 
     private String[] joueurs;
+    private CompoundButton.OnCheckedChangeListener onCheckedChangeListener = new CompoundButton.OnCheckedChangeListener(){
+        @Override
+        public void onCheckedChanged(CompoundButton button, boolean isChecked) {
 
+            }
+    };
 
-    public boolean nbrjoueurs (MenuItem item){
+    public void nbrjoueurs (){
         LinearLayout preneur = (LinearLayout) findViewById(R.id.bouttonPreneur);
-        for(int i=0; i<joueurs.length; i++){
-            Button player = new Button(this);
+        for(int i=0; i<joueurs.length; i++) {
+            ToggleButton player = new ToggleButton(this);
             player.setText(joueurs[i]);
             player.setId(i);
             preneur.addView(player);
+            player.setId(i);
+            player.setOnCheckedChangeListener(onCheckedChangeListener);
         }
-        return true;
     }
+
+    public void joueursAppelé (){
+        LinearLayout appele = (LinearLayout) findViewById(R.id.bouttonAppele);
+        for(int i=0; i<joueurs.length; i++) {
+            ToggleButton player = new ToggleButton(this);
+            player.setText(joueurs[i]);
+            player.setId(i);
+            appele.addView(player);
+            player.setId(i+joueurs.length);
+        }
+    }
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,12 +62,16 @@ public class compteur extends AppCompatActivity {
         if(intent!=null){
             joueurs = intent.getStringArrayExtra("joueurs");
         }
+        nbrjoueurs();
+        joueursAppelé();
+
     }
+
 
     @Override
     protected void onRestart(){
         super.onRestart();
-        Log.i("lifeCycle", "onRestar");
+        Log.i("lifeCycle", "onRestart");
     }
 
     @Override
